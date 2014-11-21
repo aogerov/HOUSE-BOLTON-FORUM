f3e42e43ec1af1ee0a13c2731ec858e28fd83f79
f1mp3r <craizup@gmail.com>
2014-11-21 21:07:20 +0200

    Added methods (addCategory() and deleteCategory()) to categoryModule
    Added methods (addCategory() and deleteCategory()) to categoryModule

--- scripts/Categories-API.js
+++ scripts/Categories-API.js
@@ -8,28 +8,81 @@ var categoriesModule = (function() {
 				"X-Parse-REST-API-Key": parseConstants.PARSE_REST_API_KEY
 			},
 			url: "https://api.parse.com/1/classes/Category",
-			success: handleCategories,
+			success: categoriesLoaded,
 			error: function(err) {
-				alert(err);
+				throw new Error("Could not load categories");
 			}
 		});
 	}
 
-	function handleCategories(data) {
+	function categoriesLoaded(data) {
 		var categories = data.results;
-
-		if ($('#categories').length == 0) {
-			$('body').append('<div id="categories"></div>');
-			$categories = $('#categories');
-		};
+		$categories = $('#categories').length == 0 ? $('body').append('<div id="categories"></div>') : $('#categories');
 
 		$.each(categories, function(_, category) {
 			$('<h2>').text(category.name).appendTo($categories);
-			questionsModule.getAllQuestionsFromCategory(category.objectId);
 		});
 	}
 
+	function addCategory(name) {
+		// implying we have userModule.isLoggedIn, until then ->
+		userModule = {isLoggedIn: true}
+
+		if (userModule.isLoggedIn) {
+			// We may need to add some checking on name later
+			$.ajax({
+				type: "POST",
+				headers: {
+					"X-Parse-Application-Id": parseConstants.PARSE_APPLICATION_ID,
+					"X-Parse-REST-API-Key": parseConstants.PARSE_REST_API_KEY
+				},
+				url: "https://api.parse.com/1/Category",
+				data: JSON.stringify({name: name}),
+				contentType: 'application/json',
+				dataType: 'json',
+				success: categoryAdded,
+				error: handleCategoryAddError
+			});
+		}
+	}
+
+	function categoryAdded(data) {
+		alert('Category successfully created!')
+	}
+
+	function handleCategoryAddError(error) {
+		console.log('Could not add new category: ' + error);
+		throw new Error("Could not add new category");
+	}
+
+	function deleteCategory(categoryId) {
+		if(userModule.isLoggedIn) {
+			$.ajax({
+				type: "DELETE",
+				headers: {
+					"X-Parse-Application-Id": parseConstants.PARSE_APPLICATION_ID,
+					"X-Parse-REST-API-Key": parseConstants.PARSE_REST_API_KEY
+				},
+				url: "https://api.parse.com/1/Category/" + categoryId,
+				success: categoryDeleted,
+				error: handleCategoryDeleteError
+			});
+		}
+	}
+
+	function categoryDeleted(data) {
+		alert("Category successfully deleted!");
+		console.log(data);
+	}
+
+	function handleCategoryDeleteError(err) {
+		console.log("Could not delete category!\n" + err);
+		throw new Error("Could not delete category");
+	}
+
 	return {
-		getAllCategories: getAllCategories
+		getAllCategories: getAllCategories,
+		addCategory: addCategory,
+		deleteCategory: deleteCategory
 	}
 })();
