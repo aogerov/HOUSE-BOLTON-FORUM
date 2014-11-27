1d88037fded100454e10cfffb7544e87f59675af
reathh <Ivelin Kirilov>
2014-11-27 14:20:08 +0200

    Split the Question-Answer-View into two parts - Question-View & Answer-View

--- scripts/Views/Answer-View.js
+++ scripts/Views/Answer-View.js
@@ -1,31 +1,4 @@
 var QuestionAnswerView = (function(){
-
-	// works for all questions, but should for only one, by given Id
-	// needs rework after questions-api has been updated
-	function visualize(questionId){
-		var promise = questionsModule.getQuestionByID(questionId);
-		promise.success(visualizeQuestions);
-		promise.error(function(data){
-			console.log("Cannot visualize questions");
-			console.log(data);
-		});
-	}
-	
-	function visualizeQuestions(data){
-		var question = data;
-		var questionsList = $("#questions");
-		var questionTitle = $("<h4>").text(question.title),
-			questionContent = $("<div>").text(question.content),
-			questionElement = $("<li>")
-				.attr("data-id",question.objectId)
-				.attr("data-type","question")
-				.append(questionTitle)
-				.append(questionContent)
-				.appendTo(questionsList);
-		answerView(question.objectId);
-		
-	}
-	
 	/*function visualizeQuestions(data){
 		var allQuestions = data.results.sort( function(a,b){
 			return a.createdAt > b.createdAt;
@@ -73,6 +46,7 @@ var QuestionAnswerView = (function(){
 	}
 	
 	return {
-		visualize: visualize
+		visualize: visualize,
+        answerView: answerView
 	}
 })();
--- scripts/Views/Question-View.js
+++ scripts/Views/Question-View.js
@@ -1,3 +1,26 @@
-/**
- * Created by Reath on 11/27/14.
- */
+// works for all questions, but should for only one, by given Id
+// needs rework after questions-api has been updated
+function visualize(questionId){
+    var promise = questionsModule.getQuestionByID(questionId);
+    promise.success(visualizeQuestions);
+    promise.error(function(data){
+        console.log("Cannot visualize questions");
+        console.log(data);
+    });
+}
+
+function visualizeQuestions(data){
+    var question = data;
+    var questionsList = $("#questions");
+    var questionTitle = $("<h4>").text(question.title),
+        questionContent = $("<div>").text(question.content),
+        questionElement = $("<li>")
+            .attr("data-id",question.objectId)
+            .attr("data-type","question")
+            .append(questionTitle)
+            .append(questionContent)
+            .appendTo(questionsList);
+    answerView(question.objectId);
+
+}
+	
