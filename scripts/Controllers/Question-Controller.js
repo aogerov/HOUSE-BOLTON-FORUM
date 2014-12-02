a74e8c6abd578174e128ea9203769562b7b49201
Icakis <Icakis@users.noreply.github.com>
2014-12-02 13:06:46 +0200

    add argument authorID to questionView.visualizeSmallQuestion for author href

--- scripts/Controllers/Question-Controller.js
+++ scripts/Controllers/Question-Controller.js
@@ -1,37 +1,38 @@
 var questionController = (function () {
-
-
-//    function addEventHandlerForTitleClick(element) {
-//        element.click(function () {
-//            getAndVisualiseQuestionByID($(element).attr('data-id'));
-//        });
-//    }
-
+    
+    
+    //    function addEventHandlerForTitleClick(element) {
+    //        element.click(function () {
+    //            getAndVisualiseQuestionByID($(element).attr('data-id'));
+    //        });
+    //    }
+    
     function getAndVisualizeLastNQuestions(n) {
         var mainSection = $('main');
-
+        
         mainSection.text('');
         mainSection.append($('<table id="questions">'));
         var questionsTable = $('#questions');
         questionsTable.append($('<thead>').append($('<tr><th>Title</th><th>Content</th><th>Author</th><th>Category</th><th>Tags</th><th>Visits</th><th>Votes</th></tr>')));
-
+        
         var questionsPromise = questionsModule.getAllQuestions(n);
         questionsPromise.success(function (data) {
             var questions = data.results;
-
+            
             $.each(questions, function (_, question) {
                 $.when(UserModule.getUserById(question.createdBy.objectId), CategoriesModule.getCategoryByID(question.category.objectId)).done(function (userData, categoryData) {
                     var questionTitle = question.title;
                     var questionContent = question.content.substr(0, CHARACTERS_TO_BE_DISPLAYED_AT_SMALL_QUESTIONS) + "...";
                     var questionAuthor = userData[0].username;
+                    var questionAuthorId = userData[0].objectId;
                     var questionCategory = categoryData[0].name;
                     var questionTags = "some tags";
                     var questionVisits = question.visits;
                     var questionVotes = question.votes;
-
-                    var questionHTML = questionView.visualizeSmallQuestion(question.objectId, questionTitle, questionContent, questionAuthor, questionCategory, questionTags, questionVisits, questionVotes);
+                    
+                    var questionHTML = questionView.visualizeSmallQuestion(question.objectId, questionTitle, questionContent, questionAuthor, questionCategory, questionTags, questionVisits, questionVotes, questionAuthorId);
                     var questionTitleHTML = $(questionHTML).find('.small-question-title');
-//                    addEventHandlerForTitleClick(questionTitleHTML);
+                    //                    addEventHandlerForTitleClick(questionTitleHTML);
                     questionsTable.append(questionHTML);
                 })
             });
@@ -39,10 +40,10 @@ var questionController = (function () {
             console.log(err);
         });
     }
-
+    
     function getAndVisualizeQuestionByID(questionID) {
         var mainSection = $('main');
-
+        
         mainSection.text('');
         mainSection.append($('<table id="question">'));
         var questionPromise = questionsModule.getQuestionByID(questionID);
@@ -51,11 +52,11 @@ var questionController = (function () {
             var questionTitle = question.title;
             var questionContent = question.content;
             var questionAuthor;
-            $.ajaxSetup({async: false});
+            $.ajaxSetup({ async: false });
             var responseJSONUser = UserModule.getUserById(question.createdBy.objectId).responseJSON;
             questionAuthor = responseJSONUser.username;
-            $.ajaxSetup({async: true});
-
+            $.ajaxSetup({ async: true });
+            
             $('#question').append(questionView.visualizeLargeQuestionWithAnswers(questionTitle, questionContent, questionAuthor))
         }).error(function (err) {
             console.log(err);
--- scripts/Views/Question-View.js
+++ scripts/Views/Question-View.js
@@ -1,8 +1,8 @@
 var questionView = (function () {
-    function visualizeSmallQuestion(questionID, questionTitle, questionContent, questionAuthor, questionCategory, questionTags, questionVisits, questionVotes){
+    function visualizeSmallQuestion(questionID, questionTitle, questionContent, questionAuthor, questionCategory, questionTags, questionVisits, questionVotes, questionAuthorId){
         var title = $('<a href="#/view-question/'+ questionID + '">').append($('<h4 data-id="' + questionID + '">').attr('class', 'small-question-title').text(questionTitle));
         var content = $('<span>').attr('class', 'small-question-content').text(questionContent);
-        var author = $('<span>').attr('class', 'small-question-author').text(questionAuthor);
+        var author = $('<a href="#/user/' + questionAuthorId + '">').attr('class', 'small-question-author').text(questionAuthor);
         var category = $('<span>').attr('class', 'small-question-category').text(questionCategory);
         var tags = $('<span>').attr('class', 'small-question-tags').text(questionTags);
         var visits = $('<span>').attr('class', 'small-question-visits').text(questionVisits);
