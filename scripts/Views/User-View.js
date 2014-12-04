3a460954f5d4e719557573b564213491c93bcf7a
Icakis <Icakis@users.noreply.github.com>
2014-12-04 03:32:15 +0200

    add Login and Registration to nav bar and icoCSS.css for them

--- scripts/Views/User-View.js
+++ scripts/Views/User-View.js
@@ -1,23 +1,25 @@
 ﻿'use strict';
 
 var UserView = (function () {
-    function registerView(parentContainer) {
+    function registerView() {
         function reg() {
             // cannot catch thronw errors ?!
             UserController.registerUser($('#userNameInput').val(), $('#emailInput').val(), $('#password1Input').val(), $('#password2Input').val());
         }
         
-        parentContainer = $('main');
+        var parentContainer = $('main');
         parentContainer.children().remove();
         
         // check if register container exists in DOM
         var existingRegisterContainer = $('#registerSection');
         if (existingRegisterContainer.length == 0) {
-            var registerContainer = $('<div>')
+            var registerContainer = $('<article>')
                 .attr('id', 'registerSection')
+                .attr('class', 'small-question')
                 .appendTo(parentContainer);
-            $('<label>')
-                .text('register:')
+            
+            $('<h2>')
+                .text('Registration:')
                 .attr('id', 'registerLabel')
                 .appendTo(registerContainer);
             $('<input>')
@@ -47,7 +49,7 @@ var UserView = (function () {
             $('<input>')
                 .attr('id', 'registerButton')
                 .attr('type', 'button')
-                .attr('value', 'register')
+                .attr('value', 'Register')
                 .appendTo(registerContainer)
                 .click('click', reg);
         }
@@ -61,7 +63,7 @@ var UserView = (function () {
         }
     }
     
-    function loginView(parentContainer) {
+    function loginView() {
         function login() {
             UserController.loginUser($('#userNameLoginInput').val(), $('#passwordLoginInput').val()).error(function () {
                 $('#loginMessageLabel')
@@ -70,20 +72,19 @@ var UserView = (function () {
                         .fadeOut(5000);
             });
         }
-
-        if (!parentContainer) {
-            var mainContainer = $('main');
-            mainContainer.children().remove();
-            parentContainer = $(mainContainer);
-        }
+        
+        var parentContainer = $('main');
+        parentContainer.children().remove();
+        
         
         var existingLoginContainer = $('#loginSection');
         if (existingLoginContainer.length == 0) {
-            var loginContainer = $('<div>')
+            var loginContainer = $('<article>')
                 .attr('id', 'loginSection')
+                .attr('class', 'small-question')
                 .appendTo(parentContainer);
-            $('<label>')
-                .text('login:')
+            $('<h2>')
+                .text('Login:')
                 .attr('id', 'loginLabel')
                 .appendTo(loginContainer);
             $('<input>')
@@ -101,7 +102,7 @@ var UserView = (function () {
             $('<input>')
                 .attr('id', 'loginButton')
                 .attr('type', 'button')
-                .attr('value', 'login')
+                .attr('value', 'Login')
                 .appendTo(loginContainer)
                 .click('click', login);
             $('<label>')
@@ -120,54 +121,72 @@ var UserView = (function () {
     }
     
     function userProfileView(user, isYours) {
-        var parentContainer = $('main');
-        parentContainer.children().remove();
+        var parentContainer = $('body main');
+        parentContainer
+                .children()
+                .remove();
         
         var existingUserProfileContainer = $('#userProfileSection');
         existingUserProfileContainer.remove();
-        //console.log(isYours);
         
-        //if (existingUserProfileContainer.length == 0) {
-        var userProfileContainer = $('<div>')
+        var userProfileContainer = $('<article>')
                 .attr('id', 'userProfileSection')
                 .appendTo(parentContainer);
+        
+        $('<h2>')
+                .text(user.username + ' profile:')
+                .attr('id', 'userProfileSectionHeading')
+                .appendTo(userProfileContainer);
+        
+        var profileImageContainer = $('<div>')
+                .attr('id', 'profileImageContainer')
+                .appendTo(userProfileContainer);
+        
+        var avatarImg = $('<img>')
+                .attr('id', 'profileImage')
+                .attr('alt', user.username)
+                .appendTo(profileImageContainer);
         
         var changeTag = 'label';
         if (isYours) {
             changeTag = 'input';
         }
         
+        var profileInfoContainer = $('<div>')
+                .attr('id', 'profileInfoContainer')
+                .appendTo(userProfileContainer);
+        $('<h4>')
+                .attr('id', 'personalInfoHeading')
+                .text('Personal info:')
+                .appendTo(profileInfoContainer);
         $('<label>')
                 .text('Username:')
                 .attr('id', 'userNameProfileLabel')
-                .appendTo(userProfileContainer);
+                .appendTo(profileInfoContainer);
         $('<' + changeTag + '>')
                 .attr('id', 'userNameProfileInput')
                 .text(user.username)
                 .attr('type', 'text')
                 .attr('placeholder', 'username...')
                 .attr('value', user.username)
-                .appendTo(userProfileContainer);
+                .appendTo(profileInfoContainer);
         $('<label>')
                 .text('Email:')
                 .attr('id', 'emailProfileLabel')
-                .appendTo(userProfileContainer);
+                .appendTo(profileInfoContainer);
         $('<' + changeTag + '>')
                 .attr('id', 'emailProfileInput')
                 .text(user.email)
                 .attr('type', 'email')
                 .attr('placeholder', 'email...')
                 .attr('value', user.email)
-                .appendTo(userProfileContainer);
+                .appendTo(profileInfoContainer);
         $('<label>')
                 .text('Ranking:' + user.ranking)
                 .attr('id', 'rankingProfileLabel')
-                .appendTo(userProfileContainer);
+                .appendTo(profileInfoContainer);
         
-        var avatarImg = $('<img>')
-                .attr('id', 'imageProfileLabel')
-                .attr('alt', user.username)
-                .appendTo(userProfileContainer);
+        
         
         if (!user.avatar) {
             console.log('Missing avatar');
@@ -187,11 +206,11 @@ var UserView = (function () {
                 .attr('type', 'file')
                 .attr('value', 'Change avatar')
                 .attr('accept', 'image/*')
-                .appendTo(userProfileContainer)
-                .click(user, changeAvatar);
+                .click(user, changeAvatar)
+                .appendTo(profileImageContainer);
             
             // TODO: add user role label
-            //$('<label>').text('Rank:' + userRole.name).attr('id', 'imageProfileLabel').attr('src', user.avatar).appendTo(userProfileContainer);
+            //$('<label>').text('Rank:' + userRole.name).attr('id', 'profileImage').attr('src', user.avatar).appendTo(userProfileContainer);
             
             $('<input>')
                 .attr('id', 'saveChangesButton')
@@ -199,10 +218,6 @@ var UserView = (function () {
                 .attr('value', 'Save')
                 .appendTo(userProfileContainer)
                 .click('click', saveChanges);
-            $('<label>')
-                .text('')
-                .attr('id', 'loginMessageLabel')
-                .appendTo(userProfileContainer);
         }
         
         
@@ -215,7 +230,7 @@ var UserView = (function () {
                 file = files[0];
                 UserController.uploadFile(file)
                 .success(function (upploadedFile) {
-                    $('#imageProfileLabel').attr('src', upploadedFile.url).attr('alt', user.username).attr('data-filename', JSON.stringify(upploadedFile));
+                    $('#profileImage').attr('src', upploadedFile.url).attr('alt', user.username).attr('data-filename', JSON.stringify(upploadedFile));
                 })
                 .error(function () {
                     alert('Cannot load that image.');
@@ -224,7 +239,7 @@ var UserView = (function () {
         }
         
         function saveChanges() {
-            var uploadedFile = JSON.parse($('#imageProfileLabel').attr('data-filename'));
+            var uploadedFile = JSON.parse($('#profileImage').attr('data-filename'));
             console.log(uploadedFile);
             //console.log(user);
             if (uploadedFile) {
@@ -265,40 +280,73 @@ var UserView = (function () {
     //}
     
     function showAndHideLoginLogoutRegisterIfUserIsLogged(isLogged, id, name) {
-        var userControlContainer = $('#userControlContainer');
-        if (userControlContainer.length == 0) {
-            userControlContainer = $('<div>')
-            .attr('id', 'userControlContainer')
-            .appendTo('header');
-        }
-        userControlContainer.children().remove();
+        var navUl = $('body header nav ul');
+        var registerLi = $('#registerLi');
+        var loginLi = $('#loginLi');
+        var logoutLi = $('#logoutLi');
+        var userProfileLi = $('#userProfileLi');
         
         if (isLogged) {
+            
+            if (loginLi.length !== 0) {
+                loginLi.remove();
+            }
+            
+            if (registerLi.length !== 0) {
+                registerLi.remove();
+            }
+            
+            if (logoutLi.length === 0) {
+                logoutLi = $('<li>').attr('id', 'logoutLi').appendTo(navUl);
+            }
+            
             $('<a>')
             .attr('href', "#/logout/")
             .text('Logout')
             .attr('id', 'logoutAtag')
-            .appendTo(userControlContainer);
+            .appendTo(logoutLi);
+            
+            if (userProfileLi.length === 0) {
+                userProfileLi = $('<li>').attr('id', 'userProfileLi').appendTo(navUl);
+            }
             
             $('<a>')
             .attr('href', "#/user/" + id)
             .text(name + "' profile")
             .attr('id', 'userProfile')
-            .appendTo(userControlContainer);
+            .appendTo(userProfileLi);
+
         } else {
+            if (logoutLi.length !== 0) {
+                logoutLi.remove();
+            }
+            
+            if (userProfileLi.length !== 0) {
+                userProfileLi.remove();
+            }
+            
+            if (loginLi.length === 0) {
+                loginLi = $('<li>').attr('id', 'loginLi').appendTo(navUl);
+            }
+            
             $('<a>')
             .attr('href', "#/login/")
             .text('Login')
             .attr('id', 'loginAtag')
-            .appendTo(userControlContainer);
+            .appendTo(loginLi);
+            
+            if (registerLi.length === 0) {
+                registerLi = $('<li>').attr('id', 'registerLi').appendTo(navUl);
+            }
             
             $('<a>')
             .attr('href', "#/register/")
             .text('Register')
             .attr('id', 'registerAtag')
             .css('display', 'inline-block')
-            .appendTo(userControlContainer);
+            .appendTo(registerLi);
         }
+
 
     }
     
--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,6 @@ $(function () {
     questionController.getAndVisualizeLastNQuestions(NUMBER_OF_LAST_QUESTIONS_DEFAULT);
     TagsController.getAndVisualizeTags();
     categoryController.getAndVisualizeCategories();
+	
+	UserController.checkIsUserLoggedIn();
 })
